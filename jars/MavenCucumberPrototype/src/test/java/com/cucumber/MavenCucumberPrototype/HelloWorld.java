e1b54445a840afd2eddf806b5081b6a5a90713e1
Tah, Priya <Priya.Tah@rsa.com>
2017-01-05 18:27:33 +0530

    pushing jars

---
+++ jars/MavenCucumberPrototype/src/test/java/com/cucumber/MavenCucumberPrototype/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.cucumber.MavenCucumberPrototype;
+
+/**
+ * Created by tahp1 on 1/5/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
